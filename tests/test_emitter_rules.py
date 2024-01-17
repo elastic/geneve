ef9e47b58f8a25e43527af71ac6f2dca366b55c5
Domenico Andreoli <domenico.andreoli@elastic.co>
2024-01-17 17:28:02 +0100

    Some serverless improvements

--- tests/test_emitter_rules.py
+++ tests/test_emitter_rules.py
@@ -233,6 +233,12 @@ class TestSignalsRules(tu.SignalsTestCase, tu.OnlineTestCase, tu.SeededTestCase,
             "ack_too_few_signals": 8,
             "ack_unsuccessful_with_signals": 7,
         },
+        "8.13": {
+            "ack_failed": 1,
+            "ack_no_signals": 2,
+            "ack_too_few_signals": 7,
+            "ack_unsuccessful_with_signals": 6,
+        },
     }
 
     def test_rules(self):
